bcb3a0aae934677223cc948777dfb340a4d13651
erand2015 <137082383+erand2015@users.noreply.github.com>
2026-03-09 17:38:55 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Mund ta mbash reactCompiler nëse versioni i Next.js e mbështet, 
+  // por për siguri në deploy e lëmë kështu:
   reactCompiler: true,
 };
 
